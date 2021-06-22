e8b5ffbf0043ca38046d2894bf7fde3a2d2fc4cf
crawlermust <43526339+crawlermust@users.noreply.github.com>
2021-06-22 20:28:01 -0300

    Add files via upload

---
+++ Atividade 06 Restaurantes Delivery/DeliveryRestaurantes/DeliveryRestaurantes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeliveryRestaurantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestaurantesRegiao regLondrina = new RestaurantesRegiao("Londrina");
+
+            Restaurante r1 = new Restaurante("Hot Tapioca", "Praça da Sé", "33237889");
+            Produto p1 = new Produto("Crepioca com Frango", "Massa de tapioca com ovo e recheio de frango", 15f);
+            Produto p2 = new Produto("Crepioca Doce", "Massa de tapioca com ovo e recheio de chocolate e creme de amendoin", 12.50f);
+            r1.cardapio.adicionarProduto(p1);
+            r1.cardapio.adicionarProduto(p2);
+            regLondrina.adicionarRestaurante(r1);
+
+            Restaurante r2 = new Restaurante("AçaíFreeze", "Avenida JK", "332445656");
+            Produto p3 = new Produto("Açaí ao pote", "500g", 12f);
+            Produto p4 = new Produto("Açaí com frutas", "Açaí acompanho com 5 variedades de frutas", 15.50f);
+            r2.cardapio.adicionarProduto(p3);
+            r2.cardapio.adicionarProduto(p4);
+            regLondrina.adicionarRestaurante(r2);
+
+            regLondrina.removerRestaurante(r1);
+            regLondrina.mostrarRestaurantes();
+            r2.mostrarCardapio();
+
+            RestaurantesRegiao regMaringa = new RestaurantesRegiao("Cambé");
+            Restaurante r3 = new Restaurante("Marmitaria do João", "Av Leste Oeste", "4434433245");
+            Produto p5 = new Produto("Marmita média", "Arroz com feijão 2 carnes", 13.50f);
+            Produto p6 = new Produto("Sobrmesa Sortida", "Torta de morango, ou bolo de fubá", 6.50f);
+            r3.cardapio.adicionarProduto(p5);
+            r3.cardapio.adicionarProduto(p6);
+            regMaringa.adicionarRestaurante(r3);
+
+            r3.removerProduto(p6);
+
+            regMaringa.mostrarRestaurantes();
+            r3.mostrarCardapio();
+
+            Pedido pedido1 = new Pedido("Renato");
+            pedido1.adicionarProduto(p1);
+            pedido1.adicionarProduto(p2);
+            pedido1.entregar();
+            pedido1.mostrarPedido();
+            r3.adicionarPedido(pedido1);
+            r3.criarPedido("Fernando", new Produto[]{p5});
+            r2.criarPedido("Sasuke", new Produto[] { p3, p4 });
+            r2.criarPedido("Naruto", new Produto[] { p3 });
+            r2.criarPedido("Goku", new Produto[] { p4 });
+            r3.mostrarPedidos(Restaurante.TipoPedido.NaoEntregue);
+            r3.mostrarPedidos(Restaurante.TipoPedido.Entregue);
+            r3.mostrarPedidos(Restaurante.TipoPedido.Todos);
+            r2.mostrarPedidos(Restaurante.TipoPedido.NaoEntregue);
+            r2.mostrarPedidos(Restaurante.TipoPedido.Entregue);
+            r2.mostrarPedidos(Restaurante.TipoPedido.Todos);
+        }
+    }
+}
---
+++ Atividade 06 Restaurantes Delivery/DeliveryRestaurantes/DeliveryRestaurantes/Restaurante.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeliveryRestaurantes
+{
+    class Restaurante
+    {
+        public enum TipoPedido
+        {
+            Entregue,
+            NaoEntregue,
+            Todos
+        }
+        public string nome, endereco, cnpj;
+        public Cardapio cardapio;
+        public List<Pedido> pedidos;
+        
+        public Restaurante(string nome, string endereco, string cnpj)
+        {
+            this.nome = nome;
+            this.endereco = endereco;
+            this.cnpj = cnpj;
+            this.cardapio = new Cardapio();
+            this.pedidos = new List<Pedido>();
+        }
+        public bool aceitaPedido(List<Produto> p)
+        {
+            if (p.Count > 0 && p.Count <= 5) return true;
+            return false;
+        }
+        public void mostrarCardapio()
+        {
+            Console.WriteLine($"=====Cardápio do restaurante {nome}=======");
+            foreach(Produto p in cardapio.produtos)
+            {
+                Console.WriteLine($"{p.nome} {p.descricao} {p.valor}");
+            }
+            Console.WriteLine();
+        }
+        public void adicionarProduto(Produto p)
+        {
+            this.cardapio.adicionarProduto(p);
+        }
+
+        public void removerProduto(Produto p)
+        {
+            this.cardapio.removerProduto(p);
+        }
+
+        public bool adicionarPedido(Pedido p)
+        {
+            if (this.aceitaPedido(p.produtos))
+            {
+                this.pedidos.Add(p);
+                return true;
+            }
+            return false;
+        }
+        public bool removerPedido(Pedido p)
+        {
+            return this.pedidos.Remove(p);
+        }
+        public bool criarPedido(string nomeCliente, Produto[] produtos)
+        {
+            List <Produto> listProdutos = new List<Produto>(produtos);
+            Pedido p = new Pedido(nomeCliente, listProdutos);
+            return this.adicionarPedido(p);
+        }
+        public void mostrarPedidos(TipoPedido tipo)
+        {
+            Console.WriteLine($"#####Pedidos do restaurante {nome}#####");
+            switch (tipo)
+            {
+                case TipoPedido.Entregue:
+                    Console.WriteLine("Pedidos entregados");
+                    foreach (Pedido p in this.pedidos)
+                    {
+                        if (p.entregue) p.mostrarPedido();
+                    }
+                    break;
+                case TipoPedido.NaoEntregue:
+                    Console.WriteLine("Pedidos que não foram entregados");
+                    foreach (Pedido p in this.pedidos)
+                    {
+                        if (!p.entregue) p.mostrarPedido();
+                    }
+                    break;
+                case TipoPedido.Todos:
+                    Console.WriteLine("Todos os pedidos");
+                    foreach (Pedido p in this.pedidos) p.mostrarPedido();
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
